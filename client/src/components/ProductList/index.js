d22b35bfe06fedfce982b8d617d660650b937835
nicolewallace09 <nicole.elisaw@gmail.com>
2020-08-15 16:00:33 -0700

    changes to ProductList

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,19 +1,18 @@
 import React, { useEffect } from "react";
 import { useQuery } from '@apollo/react-hooks';
-
 import ProductItem from "../ProductItem";
 import { QUERY_PRODUCTS } from "../../utils/queries";
 import spinner from "../../assets/spinner.gif"
-
-// hook functionality
-import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
-
-// importing indexdb
 import { idbPromise } from "../../utils/helpers";
+import { useSelector, useDispatch } from "react-redux";
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) =>{
+    return state;
+  });
+
+  const dispatch = useDispatch();
 
   const { currentCategory } = state;
   
